8ea3a2e66a35b380cea798112f5fd85295c5d2e7
Benjamin Croad <benjamincroadapple@gmail.com>
2022-10-15 09:51:09 +0100

    Tidied all events

--- src/pages/AllEvents.js
+++ src/pages/AllEvents.js
@@ -4,38 +4,6 @@ import EventShow from "./../components/events/EventShow";
 
 import classes from "./AllEvents.module.css";
 
-
-// const DATA = [
-//   {
-//     id: "1",
-//     title: "Title 1",
-//     description: "Description 1",
-//     location: "Bristol",
-//     price: "100"
-//   },
-//   {
-//     id: "2",
-//     title: "Title 2",
-//     description: "Description 2",
-//     location: "London",
-//     price: "20"
-//   },
-//   {
-//     id: "3",
-//     title: "Title 3",
-//     description: "Description 3",
-//     location: "York",
-//     price: "80"
-//   },
-//   {
-//     id: "4",
-//     title: "Title 4",
-//     description: "Description 4",
-//     location: "Manchester",
-//     price: "40"
-//   },
-// ];
-
 function AllEventsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedEvents, setLoadedEvents] = useState([]);
